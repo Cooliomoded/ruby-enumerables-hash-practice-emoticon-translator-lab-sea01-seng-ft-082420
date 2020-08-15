91f8ff21350dc540f2d65c81c1c1f8154d8f6f24
Davis Brown <cooliomoded@gmail.com>
2020-08-15 23:14:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,6 @@ def load_library(library)
   library_hash = YAML.load_file(library)
   emoticon_hash = {}
   library_hash.each do |expression_keys, emoticon_arrays|
-    emoticon_arrays.each do |codes|
       emoticon_hash[expression_keys] ||= {
         "english": emoticon_arrays[0],
         "japanese": emoticon_arrays[1]
@@ -18,8 +17,9 @@ end
 #emoticon_array is a value of the key known as expression_keys, it is an array that holds
 #codes, the items in the emoticon_arrays pointed to by their expression_keys.
 #
-def get_japanese_emoticon(library, emoticon)
-  new_library = load_library(library)
+def get_japanese_emoticon(pathway, emoticon)
+  load_library(pathway)
+  new_library 
   # code goes here
 end
 
